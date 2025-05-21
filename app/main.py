d1fbc6fe8c69139a1748e74110894cd7b38d8fed
rahulsheoran746 <93212370+rahulsheoran746@users.noreply.github.com>
2025-05-21 17:52:16 +0530

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -11,63 +11,8 @@ app.include_router(transaction_routes.router)
 @app.get("/")
 def read_root():
     return {"message": "Sweet Shop API is Running ✅"}
-# from app.db import get_connection
-# from app.services.transaction_service import TransactionService
 
-# sample_payload = {
-#     "member": {
-#         "name": "Rajesh Kumar",
-#         "phone": "9876543210",
-#         'village': "Village A",
-#         'state': "State A",
-#         'city': "City A"
-#     },
-#     "transactions": [
-#         {
-#             "transaction_type": "sweets",
-#             "items": [
-#                 {
-#                     "item_id": 1,
-#                     "name": "Kaju Katli",
-#                     "quantity_kg": 2.0,
-#                     "amount": 1400.00
-#                 },
-#                 {
-#                     "item_id": 3,
-#                     "name": "Motichoor Laddu",
-#                     "quantity_kg": 1.2,
-#                     "amount": 500.00
-#                 }
-#             ],
-#             "total_amount": 1900.00,
-#             "amount_given": 1500.00,
-#             "notes": "Bought sweets on credit"
-#         },
-#         {
-#             "transaction_type": "cattle_feed",
-#             "items": [
-#                 {
-#                     "item_id": 1,
-#                     "name": "abc",
-#                     "quantity_kg": 50.0,
-#                     "amount": 1800.00
-#                 }
-#             ],
-#             "total_amount": 1800.00,
-#             "amount_given": 1500.00,
-#             "notes": "Bought cattle feed  on credit"
-#         }
-#     ],
-#     "transaction_date": "2025-05-21T15:00:00Z",
-#     "description": "Transaction for Rajesh Kumar"
-# }
-
-# if __name__ == "__main__":
-#     conn = get_connection()
-#     service = TransactionService(conn)
-
-#     result = service.process_transaction_payload(sample_payload)
-#     print(result)
-
-#     conn.commit()
-#     conn.close()
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+# To run the application, use the command:
