7aaaef2462508b4c5368322bc7b806d5eab07af5
rahulsheoran746 <93212370+rahulsheoran746@users.noreply.github.com>
2025-05-21 17:55:14 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,5 +14,5 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-# To run the application, use the command:
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+
